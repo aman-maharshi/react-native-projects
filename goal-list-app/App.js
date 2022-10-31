15952bd31d68a3502ecd4ccfb83ee0bb5e573cba
aman-maharshi <amanm4harshi@gmail.com>
2022-10-31 15:57:14 +0530

    add items and show them as list functionality

--- goal-list-app/App.js
+++ goal-list-app/App.js
@@ -4,14 +4,17 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native"
 const styles = StyleSheet.create({
     appContainer: {
         paddingTop: 60,
-        paddingHorizontal: 16
+        paddingHorizontal: 16,
+        flex: 1
     },
     inputContainer: {
+        flex: 1,
         flexDirection: "row",
         justifyContent: "space-between",
-        marginBottom: 20
-        // borderWidth: 1,
-        // borderColor: "red"
+        alignItems: "center",
+        marginBottom: 20,
+        borderBottomWidth: 1,
+        borderColor: "#cccccc"
     },
     textInput: {
         flex: 1,
@@ -19,6 +22,18 @@ const styles = StyleSheet.create({
         borderColor: "#cccccc",
         marginRight: 20,
         padding: 10
+    },
+    goalsContainer: {
+        flex: 5
+    },
+    goalItem: {
+        padding: 10,
+        marginBottom: 14,
+        borderRadius: 6,
+        backgroundColor: "#5e0acc"
+    },
+    goalText: {
+        color: "white"
     }
 })
 
@@ -26,19 +41,26 @@ const App = () => {
     const [goal, setGoal] = useState("")
     const [goalList, setGoalList] = useState([])
 
-    const addGoal = () => {
-        setGoalList(prevGoals => setGoalList([goal, ...prevGoals]))
-        setGoal("")
+    const handleGoalInputChange = value => {
+        setGoal(value)
+    }
+
+    const handleAddGoal = () => {
+        setGoalList(prevGoalList => [...prevGoalList, goal])
     }
 
     return (
         <View style={styles.appContainer}>
             <View style={styles.inputContainer}>
-                <TextInput style={styles.textInput} placeholder="Your course goal!"></TextInput>
-                <Button title="Add Goal"></Button>
+                <TextInput onChangeText={handleGoalInputChange} style={styles.textInput} placeholder="Your course goal!"></TextInput>
+                <Button onPress={handleAddGoal} title="Add Goal"></Button>
             </View>
-            <View>
-                <Text>List of goals</Text>
+            <View style={styles.goalsContainer}>
+                {goalList.map(item => (
+                    <View key={item} style={styles.goalItem}>
+                        <Text style={styles.goalText}>{item}</Text>
+                    </View>
+                ))}
             </View>
         </View>
     )
